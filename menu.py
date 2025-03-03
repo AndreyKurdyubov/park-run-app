5f43d5e5398a40a1b6fc76ebd4eaa4f21850b903
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2025-03-03 11:27:20 +0300

    Add ff

--- menu.py
+++ menu.py
@@ -4,3 +4,4 @@ def menu():
     st.sidebar.page_link("pages/about.py", label="Домашняя")
     st.sidebar.page_link("pages/main_table.py", label="Таблица участников")
     st.sidebar.page_link("pages/records_table.py", label="Клубы и рекорды")
+    st.sidebar.page_link("pages/FF.py", label="Фотофиниш")
