327ffaf336ef2b89e7e3ad3ac95a3aaf8c440d6e
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2025-05-13 14:39:26 +0300

    auth at main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from utils import menu, authentication
 
-# authentication()
+authentication()
 st.switch_page('pages/home.py')  # for stramlit 1.32 - to work on iphone with old ios 
