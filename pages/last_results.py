ec66301f02a6a9ab1611008fa6c76f7c5cfd8558
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2025-07-16 11:42:00 +0300

    pandas outer join protocol

--- pages/last_results.py
+++ pages/last_results.py
@@ -16,16 +16,6 @@ authenticator, name, authentication_status, username = authentication()
 
 engine = create_engine('sqlite:///mydatabase.db')
 
-# Заголовок
-# get last run number and date
-# querie = """
-# SELECT MAX(CAST(run_number as INT)) as run_number, MAX(run_date) as run_date
-# FROM runners
-# """
-# df = pd.read_sql(querie, con=engine)
-# last_run = df['run_number'].values[0]
-# last_date = df['run_date'].values[0]
-
 querie = """
 SELECT distinct(CAST(run_number as INT)) as run_number, substr(run_date, 1, 10) as run_date
 FROM runners
@@ -43,25 +33,41 @@ list_name = 'Протокол'
 st.header(f"{list_name}\n\n**{run_select}**")
 
 querie = f'''
-SELECT 
-    r.profile_link,
-    r.position,
-    r.name,
-    r.time
-FROM runners r
-WHERE run_number = {run_number};
+SELECT profile_link, name, run_date, 
+    CAST(run_number as INT) as run_number, 
+    CAST(position as INT) as position, 
+    time,
+    achievements
+FROM runners
+WHERE run_number = {run_number}
 '''
+df_run = pd.read_sql(querie, con=engine)
 
-df = pd.read_sql(querie, con=engine)
+querie = f'''
+SELECT profile_link, name, run_date, 
+    CAST(run_number as INT) as run_number, 
+    GROUP_CONCAT(volunteer_role, ', ') as roles
+FROM organizers 
+WHERE run_number = {run_number}
+GROUP BY profile_link, run_date
+'''
+df_org = pd.read_sql(querie, con=engine)
+
+df_results = df_run.merge(df_org, how='outer', on=['profile_link', 'name', 'run_number', 'run_date']
+                  ).sort_values(by='position', ascending=True)
 
 # Отображаем таблицу
 st.data_editor(
-    df,
+    df_results,
     column_config={
         'profile_link': st.column_config.LinkColumn(label="id 5Вёрст", display_text=r"([0-9]*)$", width=''),
-        'name': st.column_config.Column(label="Имя", width='medium'), 
-        'time': st.column_config.Column(label="Время", width=''), 
-        'position': st.column_config.Column(label="Позиция", width=''), 
+        'name': st.column_config.Column(label="Участник", width='medium'), 
+        'roles': st.column_config.Column(label="Роли", width='large'),
+        'position': st.column_config.Column(label="Позиция", width=''),
+        'time': st.column_config.Column(label="Время", width=''),
+        'run_date': None,
+        'run_number': None,
+        'achievements': st.column_config.Column(label="Достижение", width='medium'),
     },
     hide_index=True
 )
@@ -69,37 +75,6 @@ st.data_editor(
 # i = 1 # button key
 # add_button(list_name, df, i)
 
-##############################################
-list_name = 'Волонтеры'
-st.header(list_name)
-
-
-querie = f'''
-SELECT 
-    o.profile_link,
-    o.name,
-    o.volunteer_role
-FROM organizers o
-WHERE run_number = {run_number};
-'''
-
-df = pd.read_sql(querie, con=engine)
-# names = df['name'].values
-
-# Отображаем таблицу
-st.data_editor(
-    df,
-    column_config={
-        'profile_link': st.column_config.LinkColumn(label="id 5Вёрст", display_text=r"([0-9]*)$", width=''),
-        'name': st.column_config.Column(label="Имя", width='medium'), 
-        'time': st.column_config.Column(label="Время", width=''),
-        'volunteer_role': st.column_config.Column(label="Роль", width=''),
-    },
-    hide_index=True
-)
-
-# i = i + 1 # button key
-# add_button(list_name, df, i)
 if username in ['host', 'org']:
     button = st.button("Отчет")
 
@@ -128,9 +103,9 @@ if username in ['host', 'org']:
 
 
         # Отображаем таблицу 
-        st.markdown(f'**Отчет {run_select}**')
-        st.markdown(f'''
-                    Количество волонтеров: {df_comb['tag'].nunique()}  
-                    ''')
-
+        st.write(f'''**Отчет {run_select}**<br>
+                     Количество финишеров: {df_results['position'].max():.0f}<br>
+                     Количество волонтеров: {df_comb['tag'].nunique()}<br>
+                     Количество уникальных участников: {len(df_results)}
+                    ''', unsafe_allow_html=True)
         st.write(dict_to_text(role_dict), unsafe_allow_html=True)
