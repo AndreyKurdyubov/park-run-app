a365b5af5373afd283e05aec8c1e2b8c813ad462
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2025-07-22 15:39:16 +0300

    test users count

--- pages/test_users.py
+++ pages/test_users.py
@@ -10,7 +10,25 @@ engine = create_engine('sqlite:///mydatabase.db')
 # Заголовок
 # get last run number and date
 querie = """
-SELECT * FROM USERS
+SELECT profile_link, name FROM USERS
 """
-df = pd.read_sql(querie, con=engine)
-st.write(len(df))
+df_users = pd.read_sql(querie, con=engine)
+
+querie = """
+SELECT distinct profile_link, name FROM runners
+WHERE profile_link LIKE "%userstats%"
+"""
+df_runs = pd.read_sql(querie, con=engine)
+
+querie = """
+SELECT distinct profile_link, name FROM organizers
+WHERE profile_link LIKE "%userstats%"
+"""
+df_orgs = pd.read_sql(querie, con=engine)
+
+df_all = df_runs.merge(df_orgs, how='outer', on=['profile_link', 'name'])
+
+st.write(f'юзеров {len(df_users)}')
+st.write(f'бегунов {len(df_runs)}')
+st.write(f'волонтеров {len(df_orgs)}')
+st.write(f'уникальных {len(df_all)}')
